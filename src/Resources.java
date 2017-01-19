be82b8ac1a21774f16702f88d6a3a75548474fd6
bogdan <deeppolis@abv.bg>
2017-01-19 10:56:26 +0000

    comments

--- src/Resources.java
+++ src/Resources.java
@@ -5,17 +5,24 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 //author
+
+
 public class Resources
 {
     public int counter = 0;
     public ArrayList<Texture> textures = new ArrayList<>();
 
+    // Precaches resources (only sprites for now).
     public Resources()
     {
         loadSpriteSheet( "dude_running.png" );
-        loadSpriteSheet( "cat_running.png" );
+        //loadSpriteSheet( "cat_running.png" );
     }
 
+    /** Loads sprite sheet
+     *
+     * @param path the relative path to the file.
+     */
     public void loadSpriteSheet( String path )
     {
         textures.add( new Texture() );
