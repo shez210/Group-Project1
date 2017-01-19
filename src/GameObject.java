539a5c7289eab7cc14fafe79097531ec5b3206c0
bogdan <deeppolis@abv.bg>
2017-01-19 10:54:10 +0000

    comments added

--- src/GameObject.java
+++ src/GameObject.java
@@ -11,11 +11,12 @@ public class GameObject
     public InputBehaviour input;
     public Vector2f velocity;
 
+    // GameObject is created by instantiating all the needed components/behaviours.
     public GameObject( Texture tex )
     {
-        sprite = new Sprite( tex );
-        anim = new AnimationBehaviour( sprite, 7, 27 );
-        input = new InputBehaviour( Game.inputHandler, this );
+        sprite = new Sprite( tex ); // Assigning a texture to sprite.
+        anim = new AnimationBehaviour( sprite, 7, 27 ); // Add animation behaviour to game object.
+        input = new InputBehaviour( Game.inputHandler, this ); // Add input behaviour to game object.
         sprite.setScale( 0.5f, 0.5f );
         velocity = new Vector2f( 0, 0 );
     }
@@ -31,9 +32,9 @@ public class GameObject
 
     void update()
     {
-        if( anim != null ) { anim.update(); }
-        if( input != null ) { input.update(); }
+        if( anim != null ) { anim.update(); } // If the GameObject supports animation, update it.
+        if( input != null ) { input.update(); } // If the GameObject is controlled by input, handle the input.
 
-        sprite.setPosition( Vector2f.add( sprite.getPosition(), velocity ) );
+        sprite.setPosition( Vector2f.add( sprite.getPosition(), velocity ) ); // moves the sprite.
     }
 }
