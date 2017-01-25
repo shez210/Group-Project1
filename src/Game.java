755926a6e3866ead5370b47bf05bc9eee9104d7a
shez210 <shez210@users.noreply.github.com>
2017-01-25 13:53:51 +0000

    Game Boundries, player info, tutorial

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,12 @@
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.system.Vector2f;
 import org.jsfml.window.VideoMode;
 import org.jsfml.window.WindowStyle;
 
 public class Game
 {
-	private static final int SCREEN_WIDTH  = 640;
-	private static final int SCREEN_HEIGHT = 480;
+	public static final int SCREEN_WIDTH  = 640;
+	public static final int SCREEN_HEIGHT = 480;
 
 	private static final String TITLE = "Game";
 
@@ -24,5 +25,11 @@ public class Game
 		window = new RenderWindow();
 		window.create( new VideoMode( SCREEN_WIDTH, SCREEN_HEIGHT ), TITLE, WindowStyle.DEFAULT );
 		Game.window.setFramerateLimit( 120 ); // Avoid excessive updates
+	}
+	public int getScreenWidth(){
+		return SCREEN_WIDTH;
+	}
+	public int getScreenHeight(){
+		return SCREEN_HEIGHT;
 	}
 }
